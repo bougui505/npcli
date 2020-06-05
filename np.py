c2cf1927c32e9275577dd474164e243dc220e33f
Guillaume Bouvier <bougui@mantrisse.bis.pasteur.fr>
2020-06-05 16:10:59 +0200

    Load data as float 	modified:   np.py

--- np.py
+++ np.py
@@ -13,5 +13,5 @@ def print(indata):
     np.savetxt(sys.stdout, indata, fmt='%.18g')
 
 
-data = np.loadtxt(sys.stdin, dtype=np.int)  # See: https://stackoverflow.com/a/8192426/1679629
+data = np.loadtxt(sys.stdin)  # See: https://stackoverflow.com/a/8192426/1679629
 exec(sys.argv[1])
