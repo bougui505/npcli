b86e956da7afe5ae93857f30fa61e949c6faab29
Guillaume Bouvier <bougui@mantrisse.bis.pasteur.fr>
2020-06-05 16:06:52 +0200

    modified:   README.md 	new file:   generate_README.sh

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # npcli
+Feed stdin data to a numpy array (default variable name is data) and apply arbitrary numpy operation on it and print the result on stdout.
+```
+$ paste =(seq 10) =(seq 11 20) | ./np.sh 'print(data)'
+
+1 11
+2 12
+3 13
+4 14
+5 15
+6 16
+7 17
+8 18
+9 19
+10 20
+```
+```
+$ paste =(seq 10) =(seq 11 20) | ./np.sh 'mu=data.mean(axis=0);print(mu)'
+
+5.5
+15.5
+```
+```
+$ paste =(seq 10) =(seq 11 20) | np 'mu=data.mean(axis=1);print(mu)'
+
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+```
---
+++ generate_README.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env zsh
+# -*- coding: UTF8 -*-
+
+# Author: Guillaume Bouvier -- guillaume.bouvier@pasteur.fr
+# https://research.pasteur.fr/en/member/guillaume-bouvier/
+# 2020-05-29 23:23:44 (UTC+0200)
+
+func runcmd () {
+    OUTPUT=$(eval $1)
+    echo "\`\`\`"
+    echo "$ $1\n"
+    echo "$OUTPUT"
+    echo "\`\`\`"
+}
+
+cat << EOF
+# npcli
+Feed stdin data to a numpy array (default variable name is data) and apply arbitrary numpy operation on it and print the result on stdout.
+EOF
+
+runcmd "paste =(seq 10) =(seq 11 20) | ./np.sh 'print(data)'"
+runcmd "paste =(seq 10) =(seq 11 20) | ./np.sh 'mu=data.mean(axis=0);print(mu)'"
+runcmd "paste =(seq 10) =(seq 11 20) | np 'mu=data.mean(axis=1);print(mu)'"
