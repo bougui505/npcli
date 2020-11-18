9ff75478228503aa689a8f82cc57278b96f5758b
Guillaume Bouvier <bougui@mantrisse.bis.pasteur.fr>
2020-11-18 12:15:09 +0100

    import scipy.spatial.distance 	modified:   np.py

--- np.py
+++ np.py
@@ -9,6 +9,7 @@ import sys
 import argparse
 import numpy as np
 import pandas as pd
+import scipy.spatial.distance as distance
 
 pd.set_option('display.max_rows', None)
 # pd.set_option('display.max_columns', None)
